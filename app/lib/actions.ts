9a61a95c8c7a806b6c606330522a9f849a80b37a
Joel Zúñiga <ext_jandzunigam@falabella.cl>
2025-08-25 15:45:39 -0400

    feat: test cache

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,22 +1,21 @@
+import { unstable_cache } from 'next/cache';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-export async function selectProducts() {
-	try {
-		await sql`
-      SELECT
-        p.id,
-        p.name,
-        p.price,
-        to_json(p.colors) AS colors,
-        COALESCE(to_json(array_agg(pi.image_url ORDER BY pi.id)), '[]') AS images
-      FROM products p
-      LEFT JOIN product_images pi ON pi.product_id = p.id
-      GROUP BY p.id
-      ORDER BY p.id;
-    `;
-	} catch (err) {
-		console.log(err);
-	}
+async function _selectProducts() {
+	console.log('[DB] selectProducts HIT REAL contra Postgres');
+	return sql`
+    SELECT p.id, p.name, p.price, to_json(p.colors) AS colors,
+           COALESCE(to_json(array_agg(pi.image_url ORDER BY pi.id)), '[]') AS images
+    FROM products p
+    LEFT JOIN product_images pi ON pi.product_id = p.id
+    GROUP BY p.id
+    ORDER BY p.id;
+  `;
 }
+
+export const selectProducts = unstable_cache(_selectProducts, ['products-all'], {
+	revalidate: 300,
+	tags: ['products'],
+});
